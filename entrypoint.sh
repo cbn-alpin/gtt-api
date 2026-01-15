786ad61162ca4f5f5347fc559a5ea8ef4716f741
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-01-15 13:49:23 +0100

    chore: dynamically change owner of migration data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+GTT_USER_UID=${GTT_USER_UID:-1000}
+GTT_USER_GID=${GTT_USER_GID:-1000}
+
 if [ "$FLASK_ENV" = "production" ]; then
+    echo "Fixing permissions for migrations/data/ content..."
+    chown -R $GTT_USER_UID:$GTT_USER_GID /home/app/web/migrations/data/
+
     echo "Running database migrations for production..."
     flask db upgrade
 fi
