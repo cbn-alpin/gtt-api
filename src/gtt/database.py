c475bf4831dd35fe489a517acd212c90d2be7bb8
Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
2026-07-02 18:06:17 +0200

    feat(database): add naming convention for constraints

--- src/gtt/database.py
+++ src/gtt/database.py
@@ -1,3 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+# Add naming convention to automatically generate constraint names.
+# This is required for Alembic to work correctly.
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+
+db = SQLAlchemy(metadata=metadata)
